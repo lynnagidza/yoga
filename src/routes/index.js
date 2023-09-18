e70cb7408405c9e6fcfcf604075c2863473faa29
Lynn Agidza <lynnagidza@gmail.com>
2023-09-18 18:07:18 +0300

    finally hacked redirecting and filtering

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.sendFile('index.html', { root: 'public' });
+  res.render('index');
 });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,34 +1,28 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
-
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
 
-const publicDirectoryPath = path.join(__dirname, '../public');
-
-app.use(express.static(publicDirectoryPath));
+app.use(express.static(path.join(__dirname, '../public')));
 
 const indexRouter = require('./routes/index');
 const productsRouter = require('./routes/products');
-const shoppingCartRouter = require('./routes/cart');
-const checkoutRouter = require('./routes/checkout');
-const orderConfirmationRouter = require('./routes/order-confirmation');
-const signinRouter = require('./routes/signin');
-const signupRouter = require('./routes/signup');
 
 app.use('/', indexRouter);
 app.use('/products', productsRouter);
-app.use('/cart', shoppingCartRouter);
-// app.use('/checkout', checkoutRouter);
-// app.use('/order-confirmation', orderConfirmationRouter);
-// app.use('/signin', signinRouter);
-// app.use('/signup', signupRouter);
+// app.use('/products', (req, res) => {
+//     console.log('Accessed /products route');
+//     res.render('products');
+// });
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
